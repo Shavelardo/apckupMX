29cc1616ebde4a9709d4502cf22f841f9998de3a
Shavelardo <31828183+Shavelardo@users.noreply.github.com>
2021-06-10 18:12:16 -0500

    nuevo todo
    dadadad

---
+++ Assets/Script/PlayMusic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.CompareTag("Objeto"))
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+        
+    }
+
+}
+    
