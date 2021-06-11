780fba10765ca98ffee9f820d993fdab113cab79
A01705742 <A01705742@itesm.mx>
2021-06-11 02:40:18 -0400

    codigo

---
+++ Assets/Script/hide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hide : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("PiedraMala"))
+        {
+
+            gameObject.SetActive(false);
+        }
+
+    }
+
+}
